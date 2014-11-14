652008bee8b2517a8a400f3434071a5a6f09666c
justin landaverde <justlandaverde28@gmail.com>
2014-11-14 15:01:11 -0800

    intial files

---
+++ desktop/src/com/landaverdej/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.landaverdej.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.landaverdej.platformer.Platformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Platformer(), config);
+	}
+}
